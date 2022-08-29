eef1773259f745558e08b28cc808223da9d3820f
toan.kien@gmail.com <toan.kien@gmail.com>
2022-08-30 09:45:35 +1000

    Updating font colour and styles so that print(Colour.red, end ="") still sets the colour Needed for the slowPrint functionality in upcoming skulpt update

--- js/console.js
+++ js/console.js
@@ -27,13 +27,16 @@ Sk.builtins.BOLD = new Sk.builtin.str("\u001b[ 1;2;0;0;0 m");
 Sk.builtins.ITALICS = new Sk.builtin.str("\u001b[ 3;2;0;0;0 m");    
 Sk.builtins.UNDERLINE = new Sk.builtin.str("\u001b[ 4;2;0;0;0 m");    
 
+
+var fontColour = "rgba(255, 255, 255, 1)";
+var fontBgColour = "rgba(0, 0, 0, 1)";
+var fontItalics = "normal";
+var fontBold = "normal";
+var fontUnderlined = "normal";
+var fontSize = "12pt";
+
 function outputf(n) {
     var text = "";
-    var color;
-    var bgcolor;
-    var italics;
-    var bold;
-    var underlined;
                   
     i = 0;
     while (n.length > 0)
@@ -54,34 +57,35 @@ function outputf(n) {
                 if (code == 0)
                 {
                     // reset
-                    color = "rgb(255,255,255)";
-                    bgcolor = "rgb(0,0,0)";                    
-                    bold = false;
-                    italics = false;
-                    underlined = false;
+                    fontColour = "rgb(255,255,255)";
+                    fontBgColour = "rgb(0,0,0)";                    
+                    fontItalics = "normal";
+                    fontBold = "normal";
+                    fontUnderlined = "normal";
+                    fontSize = "12pt";
                 }
                 else if (code == 38)
                 {
-                    color = "rgb(" + parseInt(match[2]) + "," + parseInt(match[3]) + "," + parseInt(match[4]) + ")"; 
+                    fontColour = "rgb(" + parseInt(match[2]) + "," + parseInt(match[3]) + "," + parseInt(match[4]) + ")"; 
                 }
                 else if (code == 48)
                 {
-                    bgcolor = "rgb(" + parseInt(match[2]) + "," + parseInt(match[3]) + "," + parseInt(match[4]) + ")"; 
+                    fontBgColour = "rgb(" + parseInt(match[2]) + "," + parseInt(match[3]) + "," + parseInt(match[4]) + ")"; 
                 }
                 else if (code == 1)
                 {
                     // bold
-                    bold = true;
+                    fontBold = "bold";
                 }
                 else if (code == 3)
                 {
                     // italics
-                    italics = true;
+                    fontItalics = "italic";
                 }
                 else if (code == 4)
                 {
                     // underlined
-                    underlined = true;
+                    fontUnderlined = "underline";
                 }                
                 i = match.index + match[0].length;
             }
@@ -94,7 +98,7 @@ function outputf(n) {
         }                
     }
     if (text.length > 0)
-        pyConsole.appendChild(createColouredTextSpanElement(text, color, bgcolor, italics, bold, underlined));                           
+        pyConsole.appendChild(createColouredTextSpanElement(text));                           
     
     pyConsole.scrollTop = document.getElementById("console").scrollHeight;
 }
@@ -123,73 +127,20 @@ function inputf(n) {
     return inputPromise;
 }
     
-var fontColour = "rgba(255, 255, 255, 1)";
-var fontBgColour = "rgba(0, 0, 0, 1)";
-var fontItalics = false;
-var fontBold = false;
-var fontUnderlined = false;
-var fontSize = "12pt";
-
 function resetConsole()
 {
     fontColour = "rgba(255, 255, 255, 1)";
     fontBgColour = "rgba(0, 0, 0, 1)";
-    fontItalics = false;
-    fontBold = false;
-    fontUnderlined = false;
-    fontSize = "12pt";    
+    fontItalics = "normal";
+    fontBold = "normal";
+    fontUnderlined = "normal";
+    fontSize = "12pt"; 
     clearConsole();
 }
 
-function createColouredTextSpanElement(n, color, bgcolor, italics, bold, underlined) {
+function createColouredTextSpanElement(n) {
     let t = document.createTextNode(n);        
     let e = document.createElement("span");    
-    
-    if (typeof(color) !== 'undefined')
-    {
-        fontColour = color;
-    }    
-    
-    if (typeof(bgcolor) !== 'undefined')
-    {
-        fontBgColour = bgcolor;
-    }
-    
-    if (typeof(italics) !== 'undefined')
-    {
-        if (italics)
-        {
-            fontItalics = "italic";
-        }
-        else
-        {
-            fontItalics = "normal";
-        }            
-    }
-    
-    if (typeof(bold) !== 'undefined')
-    {
-        if (bold)
-        {
-            fontBold = "bold";
-        }
-        else
-        {
-            fontBold = "normal";
-        }    
-    }    
-
-    if (typeof(underlined) !== 'undefined')
-    {
-        if (underlined)
-        {
-            fontUnderlined = "underline";
-        }
-        else
-        {
-            fontUnderlined = "none";
-        }    
-    }  
     
     e.style.textDecoration = fontUnderlined;
     e.style.fontWeight = fontBold;
@@ -200,67 +151,6 @@ function createColouredTextSpanElement(n, color, bgcolor, italics, bold, underli
     
     e.appendChild(t);        
     return e;
-}
-
-function encodeToUTF16(plaintext) {
-  var res = "";
-  for (i = 0; i < plaintext.length; i++)
-  {
-    var ch1 = plaintext.charCodeAt(i);
-    var ch2 = 0;
-    if (ch1 < 256 && i < plaintext.length - 1)
-    {
-      ch2 = plaintext.charCodeAt(++i);   
-      if (ch2 >= 256)
-      {      	
-        ch2 = 0;
-        --i;
-      }
-    }
-    else if (ch1 >= 256)
-    {
-      ch2 = 0;
-      if (i < plaintext.length - 1)
-      {
-        ch2 = plaintext.charCodeAt(++i);    
-      }
-      res = res + "@" + String.fromCharCode(ch1, ch2);
-      continue;
-    }
-    res = res + String.fromCharCode(ch1 * 256 + ch2);    
-  }
-  return res;
-}
-
-function decodeFromUTF16(codedText) {
-  var res = "";
-  
-  for (i = 0; i < codedText.length; i++)
-  {
-    ch = codedText.charCodeAt(i);
-    
-    if (ch == 64)
-    {
-      ch1 = codedText.charCodeAt(++i);
-      ch2 = codedText.charCodeAt(++i);
-      res += String.fromCharCode(ch1, ch2);
-    }
-    else
-    {
-      var ch1 = (ch / 256) % (256);
-      var ch2 = ch % (256);
-           
-      if (ch2 == 0)
-      {
-      	res += String.fromCharCode(ch1);
-      }
-      else
-      {
-      	res += String.fromCharCode(ch1, ch2);
-      }
-    }
-  }
-  return res;
 }
 
 Sk.builtins.clear = function()
